05451818e80f74aee82508762bd0d1a1437b1b8b
Manu-080 <manusankardemo@gmail.com>
2025-04-20 12:50:47 +0530

    feat : made chatroom using channels

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,9 +7,72 @@ import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        
-        await(
-            
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # modern way => self.room_group_name = f"chat{self.room_name}"
+        self.room_group_name = 'chat%s' % self.room_name 
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name,
         )
-        
-        
+
+        await self.accept()
+
+    """  TEST MESSAGE FOR LEARNING
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'test_message',
+                'tester':'hello world !',
+                'user':'admin',
+            }
+        )
+
+    
+    async def test_message(self, event):
+        tester = event['tester']
+        user = event['user']
+        print(event)
+
+        # converting dict object to json string to pass to front end.
+        await self.send(text_data=json.dumps({
+            'tester_c':tester,
+            'user':user,
+        }))
+    """
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.room_name,
+        )
+
+    # recieve message from frontend to websocket function
+    async def receive(self, text_data):
+        # converting json string to dict '{'message':message}' string  => {'message':message} dictionary
+        text_data_obj = json.loads(text_data) 
+        message = text_data_obj['message']
+        username = text_data_obj['username']
+        print(text_data_obj)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'chatroom_message',
+                'message':message,
+                'username':username,
+            }
+        )
+    
+
+    # send message to frontend function
+    async def chatroom_message(self, event):
+        message = event.get('message')
+        user = event.get('username')
+
+        # to make dict_object to json string
+        await self.send(text_data=json.dumps({
+            'message':message,
+            'username':user,
+        }))
--- core/asgi.py
+++ core/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns # import from chat(app).routing(routing.py) import websocket_urlpatterns(alternative for urlpatterns) 
+from chat.routing import websocket_urlpatterns # from chat(app).routing(routing.py) import websocket_urlpatterns(alternative for urlpatterns) 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
